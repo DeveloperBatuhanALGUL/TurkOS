f40a240e1e5d1b593f9e27106b2e1de2da653e27
Batuhan ALGÜL <batuhanalgul@proton.me>
2026-04-14 13:09:16 +0300

    interrupts.s güncellendi | Update interrupts.s
    interrupts.s güncellendi | Update interrupts.s

--- src/interrupts.s
+++ src/interrupts.s
@@ -1,58 +1,50 @@
-; ========================================================================
-; TurkOS - KESME VEKTÖRLERİ (INTERRUPT VECTORS)
-; Tanım: Her bir kesme numarası için giriş noktası sağlar.
-; İmza: Batuhan ALGÜL [Hash: 0x7F3A9B2C]
-; Yazar: Batuhan ALGÜL & TurkOS Geliştirme Ekibi
-; Tarih: 2026
-; ========================================================================
+; ==========================================
+; TurkOS Kesme Servis Rutinleri (ISR) v0.1.0
+; Telif Hakkı (c) 2026 TurkOS Geliştirme Ekibi
+; Tanım: Donanım kesmelerini C tarafına bağlayan köprü.
+; ==========================================
 
-.global isr0
-.global isr1
-.global isr32
-.global isr33
-.global islemci_kesme_yonetici
+[BITS 32]
 
-.extern c_islemci_kesme_isleyici
+; idt.c'den çağrılan fonksiyon
+global idt_load
+extern idtp ; idt.c'deki IDT işaretçisi
 
-; Ortak Kesme İşleyici Makrosu
-%macro KESME_GIRISI 1
-    pusha                   ; Tüm genel amaçlı register'ları yığına at
-    mov ax, ds              ; Data segment'i kaydet
-    push eax
-    
-    mov ax, 0x10            ; Kernel data segment selector
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    
-    push %1                 ; Kesme numarasını parametre olarak geç
-    call c_islemci_kesme_isleyici
-    add esp, 4              ; Parametreyi yığından temizle
-    
-    pop eax                 ; Data segment'i geri yükle
-    mov ds, ax
-    mov es, ax
-    mov fs, ax
-    mov gs, ax
-    
-    popa                    ; Register'ları geri yükle
-    iret                    ; Kesmeden dön
-%endmacro
-
-; Kesme Vektörleri
-isr0:
-    KESME_GIRISI 0
-
-isr1:
-    KESME_GIRISI 1
-
-isr32: ; Timer
-    KESME_GIRISI 32
-
-isr33: ; Keyboard
-    KESME_GIRISI 33
-
-; C Tarafından Çağrılan Genel İşleyici Giriş Noktası
-islemci_kesme_yonetici:
+idt_load:
+    mov eax, [esp + 4]  ; Parametre olarak gelen idtp adresini al
+    lidt [eax]          ; IDT'yi işlemciye yükle
     ret
+
+; C tarafındaki işleyicileri buraya bağlıyoruz
+extern timer_interrupt_handler
+extern keyboard_handler ; keyboard.c'deki ana fonksiyonun ismiyle uyumlu olmalı
+
+; --- KESME GİRİŞ NOKTALARI ---
+
+global irq0
+global irq1
+
+; Zamanlayıcı (Timer) - IRQ 0
+irq0:
+    pushad              ; Tüm genel amaçlı yazmaçları sakla (EAX, EBX vb.)
+    call timer_interrupt_handler
+    
+    ; PIC'e işlemin bittiğini bildir (EOI - End of Interrupt)
+    mov al, 0x20
+    out 0x20, al
+    
+    popad               ; Yazmaçları geri yükle
+    iretd               ; Kesmeden dön (Interrupt Return)
+
+; Klavye (Keyboard) - IRQ 1
+irq1:
+    pushad
+    call keyboard_handler
+    
+    mov al, 0x20
+    out 0x20, al
+    
+    popad
+    iretd
+
+; Batuhan ALGUL | "Donanım dile geldiğinde onu duymalıyız."
