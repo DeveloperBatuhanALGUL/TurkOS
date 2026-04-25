f54afd10f4c2da210d7ab2a565c92ec6eec979c2
Batuhan ALGÜL <batuhanalgul@proton.me>
2026-04-25 19:23:06 +0300

    feat(bellek): implement kernel memory map with static region table and virtual mapping

--- bellek/kmap.c
+++ bellek/kmap.c
@@ -3,90 +3,100 @@
 
 #include "kmap.h"
 #include "sanal_bellek.h"
-#include "../sürücüler/ekran/görüntü.h"
 #include "../ortak/dizge.h"
 
-static kmap_bölge_t bölgeler[KMAP_BÖLGE_SAYISI] = {
+static kmap_bölge_t bölge_tablosu[KMAP_BÖLGE_SAYISI] = {
 	{
-		KMAP_VGA_TABAN,
+		KMAP_TÜR_DÜŞÜK_BELLEK,
+		KMAP_DÜŞÜK_BELLEK_TABAN,
+		KMAP_DÜŞÜK_BELLEK_TABAN,
+		KMAP_DÜŞÜK_BELLEK_SONU - KMAP_DÜŞÜK_BELLEK_TABAN,
+		SAYFA_VAR_BİT | SAYFA_YAZILABİLİR,
+		"dusuk-bellek"
+	},
+	{
+		KMAP_TÜR_VGA,
+		KMAP_VGA_TAMPON,
+		KMAP_VGA_TAMPON,
 		KMAP_VGA_BOYUTU,
-		KMAP_TÜR_DONANIM,
-		"VGA-TAMPON",
-		DOĞRU,
-		YANLIŞ
+		SAYFA_VAR_BİT | SAYFA_YAZILABİLİR | SAYFA_ÖNBELLEK_YOK,
+		"vga-tamponu"
 	},
 	{
-		KMAP_BIOS_TABAN,
-		KMAP_BIOS_BOYUTU,
-		KMAP_TÜR_AYRILI,
-		"BIOS-ROM",
-		YANLIŞ,
-		YANLIŞ
+		KMAP_TÜR_ÇEKİRDEK,
+		KMAP_ÇEKİRDEK_TABAN,
+		KMAP_ÇEKİRDEK_TABAN,
+		KMAP_ÇEKİRDEK_AZAMI_BOYUT,
+		SAYFA_VAR_BİT | SAYFA_YAZILABİLİR,
+		"cekirdek-kodu"
 	},
 	{
-		KMAP_ÇEKIRDEK_TABAN,
-		KMAP_ÇEKIRDEK_AZAMI - KMAP_ÇEKIRDEK_TABAN,
-		KMAP_TÜR_ÇEKIRDEK,
-		"CEKIRDEK-KOD",
-		DOĞRU,
-		DOĞRU
-	},
-	{
-		KMAP_BITMAP_TABAN,
-		KMAP_BITMAP_AZAMI,
 		KMAP_TÜR_BITMAP,
-		"CERCEVE-BITMAP",
-		DOĞRU,
-		YANLIŞ
+		KMAP_CERCEVE_BİTMAP,
+		KMAP_CERCEVE_BİTMAP,
+		KMAP_CERCEVE_BİTMAP_BOYUT,
+		SAYFA_VAR_BİT | SAYFA_YAZILABİLİR,
+		"cerceve-bitmap"
 	},
 	{
+		KMAP_TÜR_YIGIN,
+		KMAP_YIGIN_TABAN,
 		KMAP_YIGIN_TABAN,
 		KMAP_YIGIN_BOYUTU,
-		KMAP_TÜR_YIGIN,
-		"YIGIN-YONETICISI",
-		DOĞRU,
-		YANLIŞ
+		SAYFA_VAR_BİT | SAYFA_YAZILABİLİR,
+		"yigin-alani"
 	},
 	{
-		KMAP_ÇERÇEVE_TABAN,
-		KMAP_ÇERÇEVE_AZAMI,
-		KMAP_TÜR_ÇERÇEVE,
-		"SERBEST-CERCEVELER",
-		DOĞRU,
-		YANLIŞ
+		KMAP_TÜR_YIGIN_ÖBEK,
+		KMAP_YIGIN_ÖBEK_TABAN,
+		KMAP_YIGIN_ÖBEK_TABAN,
+		KMAP_YIGIN_ÖBEK_BOYUTU,
+		SAYFA_VAR_BİT | SAYFA_YAZILABİLİR,
+		"yigin-obek"
 	},
-	{ 0, 0, KMAP_TÜR_BOŞLUK, "BOS", YANLIŞ, YANLIŞ },
-	{ 0, 0, KMAP_TÜR_BOŞLUK, "BOS", YANLIŞ, YANLIŞ },
+	{
+		KMAP_TÜR_MMIO,
+		KMAP_MMIO_TABAN,
+		KMAP_MMIO_TABAN,
+		KMAP_MMIO_SONU - KMAP_MMIO_TABAN,
+		SAYFA_VAR_BİT | SAYFA_YAZILABİLİR | SAYFA_ÖNBELLEK_YOK,
+		"mmio-alani"
+	},
+	{
+		KMAP_TÜR_KULLANICI,
+		SANAL_KULLANICI_TABAN,
+		SANAL_KULLANICI_TABAN,
+		SANAL_KULLANICI_SONU - SANAL_KULLANICI_TABAN,
+		SAYFA_VAR_BİT | SAYFA_YAZILABİLİR | SAYFA_KULLANICI,
+		"kullanici-alani"
+	}
 };
 
-static uint8_t	bölge_sayısı = 6;
-
-static void bölgeyi_eşle(const kmap_bölge_t *bölge)
-{
-	uint64_t bayraklar = SANAL_EŞLE_ÇEKIRDEK;
-	if (!bölge->yazılabilir)
-		bayraklar = SANAL_EŞLE_SALT_OKUNUR;
-
-	sanal_eşle(
-		çekirdek_sanal_uzay_al(),
-		bölge->taban,
-		bölge->taban,
-		bölge->boyut,
-		bayraklar);
-}
+static uint8_t kmap_hazır = YANLIŞ;
 
 void kmap_başlat(void)
 {
-	for (uint8_t i = 0; i < bölge_sayısı; i++)
-		bölgeyi_eşle(&bölgeler[i]);
+	sanal_adres_uzayı_t *uzay = çekirdek_sanal_uzay_al();
+
+	for (uint8_t i = 0; i < KMAP_BÖLGE_SAYISI; i++) {
+		kmap_bölge_t *b = &bölge_tablosu[i];
+		if (b->tür == KMAP_TÜR_KULLANICI)
+			continue;
+		sanal_eşle(uzay,
+			b->sanal_taban,
+			b->gerçek_taban,
+			b->boyut,
+			b->bayraklar);
+	}
+
+	kmap_hazır = DOĞRU;
 }
 
-const kmap_bölge_t *kmap_bölge_bul(uintptr_t adres)
+const kmap_bölge_t *kmap_bölge_al(kmap_tür_t tür)
 {
-	for (uint8_t i = 0; i < bölge_sayısı; i++) {
-		if (adres >= bölgeler[i].taban &&
-		    adres < bölgeler[i].taban + bölgeler[i].boyut)
-			return &bölgeler[i];
+	for (uint8_t i = 0; i < KMAP_BÖLGE_SAYISI; i++) {
+		if (bölge_tablosu[i].tür == tür)
+			return &bölge_tablosu[i];
 	}
 	return BOŞ;
 }
@@ -94,41 +104,28 @@ const kmap_bölge_t *kmap_bölge_bul(uintptr_t adres)
 const kmap_bölge_t *kmap_tüm_bölgeler(uint8_t *adet)
 {
 	if (adet)
-		*adet = bölge_sayısı;
-	return bölgeler;
+		*adet = KMAP_BÖLGE_SAYISI;
+	return bölge_tablosu;
 }
 
 uint8_t kmap_adres_geçerli_mi(uintptr_t adres)
 {
-	return kmap_bölge_bul(adres) != BOŞ;
+	for (uint8_t i = 0; i < KMAP_BÖLGE_SAYISI; i++) {
+		const kmap_bölge_t *b = &bölge_tablosu[i];
+		if (adres >= b->sanal_taban &&
+		    adres <  b->sanal_taban + b->boyut)
+			return DOĞRU;
+	}
+	return YANLIŞ;
 }
 
-void kmap_bölge_yazdır(void)
+kmap_tür_t kmap_adres_türü(uintptr_t adres)
 {
-	görüntü_renk_ayarla(
-		GEA_RENK(GEA_RENK_AÇIK_GRİ, GEA_RENK_SİYAH));
-	görüntü_dizge_yaz("[KMAP] Bellek bolgeleri:\n");
-
-	for (uint8_t i = 0; i < bölge_sayısı; i++) {
-		char tampon[17];
-
-		görüntü_renk_ayarla(
-			GEA_RENK(GEA_RENK_AÇIK_MAVİ, GEA_RENK_SİYAH));
-		görüntü_dizge_yaz("  ");
-		görüntü_dizge_yaz(bölgeler[i].ad);
-		görüntü_dizge_yaz(": ");
-
-		görüntü_renk_ayarla(
-			GEA_RENK(GEA_RENK_BEYAZ, GEA_RENK_SİYAH));
-		görüntü_dizge_yaz("0x");
-		onaltılı_dizgeye(bölgeler[i].taban, tampon,
-			sizeof(tampon));
-		görüntü_dizge_yaz(tampon);
-		görüntü_dizge_yaz(" - 0x");
-		onaltılı_dizgeye(
-			bölgeler[i].taban + bölgeler[i].boyut,
-			tampon, sizeof(tampon));
-		görüntü_dizge_yaz(tampon);
-		görüntü_karakter_yaz('\n');
+	for (uint8_t i = 0; i < KMAP_BÖLGE_SAYISI; i++) {
+		const kmap_bölge_t *b = &bölge_tablosu[i];
+		if (adres >= b->sanal_taban &&
+		    adres <  b->sanal_taban + b->boyut)
+			return b->tür;
 	}
+	return KMAP_TÜR_KULLANICI;
 }
